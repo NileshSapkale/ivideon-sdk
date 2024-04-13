93a24b56bb2919dc8fcb9273cea33c1f3250972c
Nilesh Sapkale <nileshsapkalevmukti@gmail.com>
2024-04-14 00:25:28 +0530

    for learning

---
+++ A1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+   char str[]={'A','E','I','O','U','a','e','i','o','u'};char Ustr[0];
+   printf("Enter Character: ");
+   scanf("%c",Ustr);
+    int i=0,check=1;
+   while(i!=9)
+   {
+       if(str[i]==Ustr[0])
+       {
+           check=0;
+           break;
+       }
+       else
+       {
+           check=1;
+       }
+       i++;
+   }
+  
+   if(check!=1)
+   {
+       printf("Charcter is Vowel! \n");
+   }
+   else
+   {
+       printf("Character is Consonant! \n");
+   }
+   return 0;
+}
